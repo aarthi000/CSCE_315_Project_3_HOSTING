178fe52d0fc0e32b4089718c2e8399e43d04fff2
mj591806 <mj591806@tamu.edu>
2022-11-06 18:31:32 -0600

    Added inputs for ManagerWindow

--- src/components/ManagerWindow.jsx
+++ src/components/ManagerWindow.jsx
@@ -19,7 +19,88 @@ class ManagerWindow extends Component {
                         <button class="role-button">Restock</button>
                         <button class="role-button">Add-ons</button>
                     </div>
+
                     <h2>Add Menu Item</h2>
+                    <div className='add-menu-item'>
+                        <div className='input-class'>
+                            <form>
+                                <label>
+                                    <input type="text" placeholder="Enter menu item name" />
+                                    <br></br>
+                                    <input type="text" placeholder="Enter menu item price" />
+                                    <br></br>
+                                    <input type="text" placeholder="Enter menu item ingredients" />
+                                    <br></br>
+                                </label>
+                            </form>
+                        </div>
+                        <div className='submit-class'>
+                            <button className='submit-btn'>+</button>
+                        </div>
+                    </div> 
+
+                    <h2>Edit Price</h2>
+                    <div className='edit-price'>
+                        <div className='input-class'>
+                            <form>
+                                <label>
+                                    <select name='menu-items'>
+                                        <option value='temp1'>temp1</option>
+                                        <option value='temp2'>temp2</option>
+                                        <option value='temp3'>temp3</option>
+                                        <option value='temp4'>temp4</option>
+                                        <option value='temp5'>temp5</option>
+                                    </select>
+                                    <br></br>
+                                    <input type="text" placeholder="Enter new menu item price" />
+                                    <br></br>
+                                </label>
+                            </form>
+                        </div>
+                        <div className='submit-class'>
+                            <button className='submit-btn'>+</button>
+                        </div>
+                    </div>
+
+                    <h2>Restock Ingredient</h2>
+                    <div className='restock-ingredient'>
+                        <div className='input-class'>
+                            <form>
+                                <label>
+                                    <select name='menu-items'>
+                                        <option value='temp1'>temp1</option>
+                                        <option value='temp2'>temp2</option>
+                                        <option value='temp3'>temp3</option>
+                                        <option value='temp4'>temp4</option>
+                                        <option value='temp5'>temp5</option>
+                                    </select>
+                                    <br></br>
+                                    <input type="text" placeholder="Enter ingredient quantity" />
+                                    <br></br>
+                                </label>
+                            </form>
+                        </div>
+                        <div className='submit-class'>
+                            <button className='submit-btn'>+</button>
+                        </div>
+                    </div>
+
+                    <h2>Add Ingredient</h2>
+                    <div className='add-ingredient'>
+                        <div className='input-class'>
+                            <form>
+                                <label>
+                                    <input type="text" placeholder="Enter ingredient name" />
+                                    <br></br>
+                                    <input type="text" placeholder="Enter ingredient quantity" />
+                                    <br></br>
+                                </label>
+                            </form>
+                        </div>
+                        <div className='submit-class'>
+                            <button className='submit-btn'>+</button>
+                        </div>
+                    </div>
                 </div>
             </section>
         );
