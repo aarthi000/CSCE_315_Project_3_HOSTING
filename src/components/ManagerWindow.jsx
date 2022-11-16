3c06d1299b711f42090454173ad0885efa66032b
mj591806 <mj591806@tamu.edu>
2022-11-16 14:07:40 -0600

    Revert "Added table component and created temporary data"
    This reverts commit 821fb49da3f76a1945bc93c2372cef0fb535ad44.

--- src/components/ManagerWindow.jsx
+++ src/components/ManagerWindow.jsx
@@ -159,7 +159,6 @@ class ManagerWindow extends Component {
             <section className='ManagerWindow'>
                 <div className='left-half'>
                     <h1>Inventory</h1>
-                    <Table></Table>
                 </div>
                 <div className='right-half'>
                     <div class="report-btn-group">
