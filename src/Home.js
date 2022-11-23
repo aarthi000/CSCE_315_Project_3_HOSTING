75a92a75016552a7aa494b2c39c31020fd4e43db
“hdhulipala02” <hdhulipala02@tamu.edu>
2022-11-22 21:49:11 -0600

    oauth_configuration_comments

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,72 @@
+// import React from "react";
+// import logo from './images/revs.png';
+// import './Home.css';
+// import { useNavigate } from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import jwt_decode from "jwt-decode";
+
+// function Home() {
+
+// 	const [user, setUser] = useState({});
+
+//     useEffect(() => {
+// 		/* global google */
+// 		google.accounts.id.initialize({
+// 			client_id: "150015526860-t3v7iu43ajjcu4mehrv7cqsip7q234qd.apps.googleusercontent.com",
+// 			callback: clickManager
+// 		});
+
+// 		google.accounts.id.renderButton(
+// 			document.getElementById("signInDiv"),
+// 			{theme: 'outline', size: 'small', width:'100px'}
+// 		);
+//     }, []);
+
+//     const navigate = useNavigate();
+
+//     function clickCustomer () {
+//         navigate("/customer")
+//     }
+//     function clickServer () {
+//         navigate("/server")
+//     }
+//     function clickManager (response) {
+// 		console.log("Encoded JWT ID Token: " + response.credential);
+// 		var userObject = jwt_decode(response.credential);
+// 		console.log(userObject);
+// 		setUser(userObject);
+// 		document.getElementById("signInDiv").hidden = true;
+//         navigate("/manager")
+//     }
+
+// 	function handleSignout(event) {
+// 		setUser({});
+// 		document.getElementById("signInDiv").hidden = false;
+
+// 	}
+
+
+
+//     return (
+//         <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <div className="btn-group">
+//             <button className="role-button" onClick={clickCustomer}>Customer</button>
+//             <button className="role-button" onClick={clickServer}>Server</button>
+//             {/* <button class="role-button" onClick={clickManager}>Manager</button> */}
+// 			<button className="role-button" id="signInDiv"></button>
+// 			{ Object.keys(user).length !== 0 &&
+// 				<button onClick = { (e) => handleSignout(e)}>Sign Out</button>
+// 			}
+//           </div>
+//         </header>
+//       </div>        
+//     )
+// }
+
+// export default Home;
+
 import React from "react";
 import logo from './images/revs.png';
 import './Home.css';
